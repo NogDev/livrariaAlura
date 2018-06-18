b096b5a76c620712267c6bddce4304230363f6d4
NogDev <anconceicao@dcc.ufba.br>
2018-06-18 18:30:08 -0300

    Implementando o CRUD para a view autor e modificando para jsf 2.2

--- livraria/src/br/com/caelum/livraria/bean/AutorBean.java
+++ livraria/src/br/com/caelum/livraria/bean/AutorBean.java
@@ -1,12 +1,19 @@
 package br.com.caelum.livraria.bean;
 
+import java.io.Serializable;
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 
 import br.com.caelum.livraria.dao.DAO;
 import br.com.caelum.livraria.modelo.Autor;
 
 @ManagedBean
-public class AutorBean {
+@ViewScoped
+public class AutorBean implements Serializable{
+
+	private static final long serialVersionUID = 1L;
 
 	private Autor autor = new Autor();
 
@@ -14,13 +21,30 @@ public class AutorBean {
 		return autor;
 	}
 
-	public String gravar() {
+	public void gravar() {
 		System.out.println("Gravando autor " + this.autor.getNome());
-
-		new DAO<Autor>(Autor.class).adiciona(this.autor);
+		
+		if (this.autor.getId() == null ) {
+			new DAO<Autor>(Autor.class).adiciona(this.autor);
+		}else{
+			new DAO<Autor>(Autor.class).atualiza(this.autor);
+		}
 		
 		this.autor = new Autor();
 		
-		return "livro?faces-redirect=true";
+	}
+	
+	public List<Autor> getAutores() {
+        return new DAO<Autor>(Autor.class).listaTodos();
+	}
+	
+	public void carregar(Autor autor){
+		System.out.println("Carregando o autor");
+		this.autor = autor; 
+	}
+	
+	public void remover(Autor autor){
+		System.out.println("Removendo Autor");
+		new DAO<Autor>(Autor.class).remove(autor);//não entendo pq não utiliza o this
 	}
 }
