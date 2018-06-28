50c898def542c31ad4b1b797d7ae0db5ef2236db
NogDev <anconceicao@dcc.ufba.br>
2018-06-28 17:04:41 -0300

    Implementando a opção de seleção de todos os temas do primefaces

--- livraria/src/br/com/caelum/livraria/bean/TemaBean.java
+++ livraria/src/br/com/caelum/livraria/bean/TemaBean.java
@@ -8,6 +8,17 @@ import javax.faces.bean.SessionScoped;
 public class TemaBean {
 	
 	private String tema ="vader";
+	
+	public String[] getTemas() {
+	    return new String[] { "afterdark", "afternoon", "afterwork", "aristo",
+	            "black-tie", "blitzer", "bluesky", "bootstrap", "casablanca",
+	            "cupertino", "cruze", "dark-hive", "delta", "dot-luv",
+	            "eggplant", "excite-bike", "flick", "glass-x", "home",
+	            "hot-sneaks", "humanity", "le-frog", "midnight", "mint-choc",
+	            "overcast", "pepper-grinder", "redmond", "rocket", "sam",
+	            "smoothness", "south-street", "start", "sunny", "swanky-purse",
+	            "trontastic", "ui-darkness", "ui-lightness", "vader" };
+	}
 
 	public String getTema() {
 		return tema;
